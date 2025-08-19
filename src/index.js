237bca2dffc4785294f6936ae6a3973745712b46
kusuma <kusumakalla12@gmail.com>
2025-08-19 20:47:01 +0530

    Add function to fetch data from apii

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import { displayDropDown,otherClick } from "./showDropDown";
-import "./styles.css";
-console.log("Hi");
-window.displayDropDown = displayDropDown;
-window.otherClick = otherClick;
-window.addEventListener("click",otherClick);
+
+async function getWeather(location){
+    let url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + location + "?unitGroup=us&key=2Q2VVKA8LL3JFCFJFNDH9J2Q8&contentType=json"
+    try{
+        let data = await fetch(url , {mode : 'cors'});
+        if(!data.ok)
+            throw new Error(`Error : ${data.status} . Please enter a valid city`);
+        let dataJson = await data.json();
+        console.log(dataJson);
+        console.log(dataJson.currentConditions.conditions);
+    } catch(error){
+        console.log(`oops error : ${error.message}`);
+    }
+}
+
+
+
+getWeather("bangalore")
